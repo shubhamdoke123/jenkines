5e845f07ca06354500f83bbbfe022207f0de1402
shubhamdoke123 <149041012+shubhamdoke123@users.noreply.github.com>
2024-01-18 12:22:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
 <<<<<<< HEAD
- res.send('hello shubham');
+ res.send('hello sd');
 =======
  res.send('hello sd');
 >>>>>>> origin/master
